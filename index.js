78ff13150f7aa938e8a69e161a3a683c57653e7c
Vinícius Melo <viniciusldemelo@gmail.com>
2026-06-15 22:54:14 -0300

    ajuste para deploy

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import { createServer } from 'node:http'
 
-const hostname = '127.0.0.1'
-const port = 3000
+const hostname = '0.0.0.0'
+const port = process.env.PORT || 3000
 
 const server = createServer((req, res) => {
     res.statusCode = 200
